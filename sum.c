a53ecdd7bb1bda4607267a64de48940846f97e88
sathishmodernchip <106298521+sathishmodernchip@users.noreply.github.com>
2022-05-26 11:49:32 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,c;
+printf("enter number1,number2");
+scanf("%d ",&num2);
+scanf("%d ",&num1);
+c=num1+num2;
+printf("sum of two numbers is %d",c);
+}
